ee5ace567a6920222043c62d0091443d19204aba
leon <leon00albert@gmail.com>
2023-05-17 18:26:50 +0300

    feat: add db create for /students/new

--- server.php
+++ server.php
@@ -1,9 +1,10 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
-
+use Artemis\Core\DataBases\DB;
 use Artemis\Core\Router\Router;
-$app = Router::getInstance();
 
+$app = Router::getInstance();
+$db = new DB("JSON","students");
 $app->get("/", function($req,$res){
     $res->render(__DIR__ . "/src/index.html");
     $res->status(200);
@@ -16,6 +17,14 @@ $app->get("/students/new", function($req,$res){
       
 });
 
+$app->post("/students", function($req,$res){
+    global $db;
+    $data = $req->body();
+    $db->con->create($data);
+    $res->json($data);
+    $res->status(200);
+      
+});
 
 $app->get("/public/:file", function($req,$res){
     $path_to_file = explode("/",$req->path())[2];
