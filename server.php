4b6abb37628e679d2dac506a9198b68de96bcca9
leon <leon00albert@gmail.com>
2023-05-18 12:49:18 +0300

    feat: add log tables and fetch /api/logs

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__."/vendor/autoload.php";
 use Artemis\Core\DataBases\DB;
 use Artemis\Core\Router\Router;
 use Artemis\Core\Forms\Forms;
@@ -44,12 +44,12 @@ $app->get("/students",function($req,$res){
             $desc = (bool) $req->query()["desc"];
             $data = sortByKey($data,$req->query()["sortby"],$desc);
         }
-        $res->json(["alerts" => end($alert) ?? '',"data" => $data]);
+        $res->json(["alerts" => end($alert) ?? "","data" => $data]);
         $res->status(200);
         $alerts->con->deleteMany([]);
     }   
     catch(Exception $e) {
-        $alerts->con->create(["alert" => ["type" => 'danger','message' => 'ERROR: could not get students']]);
+        $alerts->con->create(["alert" => ["type" => "danger","message" => "ERROR: could not get students"]]);
         $res->status(500);
     }
 
@@ -59,12 +59,12 @@ $app->get("/students/:id", function($req,$res){
     global $alerts;
     try {
   
-        $data = $db->con->findById($req->params()['id']);
+        $data = $db->con->findById($req->params()["id"]);
         $res->json($data);
         $res->status(200);
     }
     catch(Exception $e) {
-        $alerts->con->create(["alert" => ["type" => 'danger','message' => 'ERROR: could not get student with id ' . $req->params()['id']]]);
+        $alerts->con->create(["alert" => ["type" => "danger","message" => "ERROR: could not get student with id " . $req->params()["id"]]]);
         $res->status(500);
     }
 });
@@ -72,14 +72,19 @@ $app->get("/students/:id", function($req,$res){
 $app->put("/students/:id", function($req,$res){
     global $db;
     global $alerts;
+    global $logs;
     try {
-        $data = $db->con->updateById($req->params()['id'],$req->body());
-        $alerts->con->create(["alert" => ["type" => 'success','message' => 'Succesfully updated student with id ' . $req->params()['id']]]);
+        $data = $db->con->updateById($req->params()["id"],$req->body());
+        $logs->con->create(["level" => "info" , "source" => "UPDATE /students/" .$req->params()["id"] , "date" => date("D M j G:i:s T Y"),"message" => "updated student with id " . $req->params()["id"] ] );
+
+        $alerts->con->create(["alert" => ["type" => "success","message" => "Succesfully updated student with id " . $req->params()["id"]]]);
         $res->json($data);
         $res->status(200);
     }
     catch(Exception $e) {
-        $alerts->con->create(["alert" => ["type" => 'danger','message' => 'ERROR: could not update student']]);
+        $logs->con->create(["level" => "danger" , "source" => "UPDATE /students/" .$req->params()["id"] , "date" => date("D M j G:i:s T Y"),"message" => "ERROR: could not update student" ] );
+
+        $alerts->con->create(["alert" => ["type" => "danger","message" => "ERROR: could not update student"]]);
         $res->status(500);
     }
 });
@@ -87,14 +92,18 @@ $app->put("/students/:id", function($req,$res){
 $app->delete("/students/:id", function($req,$res){
     global $db;
     global $alerts;
+    global $logs;
     try {
-        $data = $db->con->deleteById($req->params()['id']);
-        $alerts->con->create(["alert" => ["type" => 'success','message' => 'Succesfully deleted student with id ' . $req->params()['id']]]);
+        $data = $db->con->deleteById($req->params()["id"]);
+        $logs->con->create(["level" => "info" , "source" => "DELETE /students/" .$req->params()["id"] , "date" => date("D M j G:i:s T Y"),"message" => "deleted student with id" . $req->params()["id"]  ] );
+
+        $alerts->con->create(["alert" => ["type" => "success","message" => "Succesfully deleted student with id " . $req->params()["id"]]]);
         $res->json($data);
         $res->status(200);
     }
     catch(Exception $e) {
-        $alerts->con->create(["alert" => ["type" => 'danger','message' => 'ERROR: could not delete student']]);
+        $logs->con->create(["level" => "danger" , "source" => "DELETE /students/" .$req->params()["id"] , "date" => date("D M j G:i:s T Y"),"message" => "ERROR: could not delete student" ] );
+        $alerts->con->create(["alert" => ["type" => "danger","message" => "ERROR: could not delete student"]]);
         $res->status(500);
     }
       
@@ -111,15 +120,15 @@ $app->post("/students",$form->sanatize,function($req,$res){
     try {
         $data = $req->body();
         $db->con->create($data);
-        $alerts->con->create(["alert" => ["type" => 'success','message' => 'Succesfully created student']]);
-        $logs->con->create(["level" => "info" , "source" => 'POST /students', 'date' => date("D M j G:i:s T Y"),'message' => 'Succesfully created student' ] );
+        $alerts->con->create(["alert" => ["type" => "success","message" => "Succesfully created student"]]);
+        $logs->con->create(["level" => "info" , "source" => "POST /students", "date" => date("D M j G:i:s T Y"),"message" => "created student" ] );
         $res->json($data);
         $res->status(200);
     }
 
     catch(Exception $e) {
-        $logs->con->create(["level" => "danger" , "source" => 'POST /students', 'date' => date("D M j G:i:s T Y"),'message' => 'ERROR: could not create student' ] );
-        $alerts->con->create(["alert" => ["type" => 'danger','message' => 'ERROR: could not create student']]);
+        $logs->con->create(["level" => "danger" , "source" => "POST /students", "date" => date("D M j G:i:s T Y"),"message" => "ERROR: could not create student" ] );
+        $alerts->con->create(["alert" => ["type" => "danger","message" => "ERROR: could not create student"]]);
         $res->status(500);
     }
 
@@ -146,7 +155,7 @@ $app->get("/api/logs",function ($req, $res) {
     try {
         $data = $logs->con->find([]);
         $alert = $alerts->con->find([]);
-        $res->json(["alerts" => end($alert) ?? '',"data" => $data]);
+        $res->json(["alerts" => end($alert) ?? "","data" => $data]);
         $res->status(200);
         $alerts->con->deleteMany([]);
     }   
