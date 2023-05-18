7df2765cfa5825a432054ec064eb7f4da3cd926f
leon <leon00albert@gmail.com>
2023-05-18 17:15:58 +0300

    feat: add feature to seed data

--- server.php
+++ server.php
@@ -208,6 +208,48 @@ $app->get("/api/logs",function ($req, $res) {
     }
 });
 
+$app->get("/api/seed",function ($req, $res) {
+    global $db;
+    global $logs;
+    global $alerts; 
+    if(isset($req->query()["count"])) {
+        $count = $req->query()["count"];
+        try {
+            $url = "https://randomuser.me/api/?results=$count&nat=us";
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($ch);
+            curl_close($ch);
+            $data = json_decode($response, true);
+
+            $results = [];
+
+            foreach($data['results'] as $item){
+
+                $input = ["name" => $item['name']['first']. " " .$item['name']['last'] , 'grade' => rand(0,10) , 'class' => "test"];
+                $db->con->create($input);
+                array_push( $results, $input);
+              
+            }
+    
+            $logs->con->create(["level" => "info" , "source" => "GET /api/seed", "date" => date("D M j G:i:s T Y"),"message" => "Created $count students" ] );
+            $alerts->con->create(["alert" => ["type" => "success","message" => "Created $count students"]]);
+    
+            $res->status(200);
+            $res->json($results);
+          
+        }   
+        catch(Exception $e) {
+            $logs->con->create(["level" => "danger" , "source" => "GET /api/seed", "date" => date("D M j G:i:s T Y"),"message" => "ERROR: could not create students" ] );
+            $alerts->con->create(["alert" => ["type" => "danger","message" => "ERROR: could not create students"]]);
+            $res->status(500);
+        }
+    } else {
+        $res->send("include ?count=x");
+    }
+   
+  
+});
 
 $app->get("/api/report",function ($req, $res) {
     global $logs;
