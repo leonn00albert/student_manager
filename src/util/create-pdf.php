b7947ee1a64cd272c06211c35855cc4a403bbb74
leon <leon00albert@gmail.com>
2023-05-20 21:46:21 +0300

    fix: create pdf with right col count based on header count

--- src/util/create-pdf.php
+++ src/util/create-pdf.php
@@ -21,8 +21,10 @@ function BasicTable($header, $data)
     foreach($header as $col)
         $this->Cell(40,7,$col,1);
     $this->Ln();
+ 
     foreach($data as $row)
     {
+        $row = array_slice($row,0,count($header));
         foreach($row as $col)
             $this->Cell(40,6,$col,1);
         $this->Ln();
