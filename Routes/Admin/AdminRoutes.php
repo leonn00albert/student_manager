0bbd1a3a1cd7f61bbef97e437c1c6fd903f047cd
leon <leon00albert@gmail.com>
2023-07-04 09:32:46 +0300

    feat: add create books for libray admin sie

--- Routes/Admin/AdminRoutes.php
+++ Routes/Admin/AdminRoutes.php
@@ -2,6 +2,8 @@
 
 namespace Routes\Admin;
 
+use Exception;
+
 class AdminRoutes
 {
     static function register($app, $controller, $db=null)
@@ -19,6 +21,38 @@ class AdminRoutes
             $res->status(HTTP_200_OK);
         });
 
+        $app->get("/admin/library", function ($req, $res) use ($db){
+            $query = [
+                "sql" => "SELECT * FROM library"
+            ];
+            $data = [
+                "template" => "library.php",
+                "books" => $db->find($query)
+            ];
+            $res->render("admin/index", $data);
+            $res->status(HTTP_200_OK);
+        });
+
+        $app->post("/admin/library",$app->form->sanitize, function ($req, $res) use ($db) {
+            $query = [
+                "sql" => "INSERT INTO library (book_image, book_title, book_url) VALUES (:book_image, :book_title, :book_url)",
+                "params" => [
+                    "book_image" => $req->sanitized["book_image"],
+                    "book_title" => $req->sanitized["book_title"],
+                    "book_url" => $req->sanitized["book_url"],
+                ]
+                ];
+                try {
+                    $db->create($query);
+                    $res->status(HTTP_201_CREATED);
+                    $res->redirect("/admin/library");
+                }
+                catch(Exception $e){
+
+                }
+     
+            
+        });
 
         $app->get("/admin/teachers", function ($req, $res) use ($db) {
             $query = [
