3f02b23243bf14682c3ad584f740bd5013e9b778
leon <leon00albert@gmail.com>
2023-07-04 09:51:50 +0300

    refactor: move teacher routes to Routes/Teachers

--- server.php
+++ server.php
@@ -20,146 +20,52 @@ $app->set("view_engine", new TemplateEngine(__DIR__ . "/views"));
 $app->use("form", new Forms());
 $app->use("notification", new Notification($db));
 
-//routes
-//AUTH ROUTES
-
-
-/// TODO CREATE CONSTANTS FOR STASTUSCODES AND VARIABLES
-
+// Register and Login
 Routes\Auth\UsersRoutes::register($app, new Controllers\Auth\UsersController());
+
+
+//ADMIN ROUTES
 if (isset($_SESSION[USER_TYPE]) && $_SESSION[USER_TYPE] === TYPE_ADMIN) {
-    //ADMIN ROUTES --TODO add auth for admin only
+
     Routes\Admin\UsersRoutes::register($app, new Controllers\Admin\UsersController());
     Routes\Admin\ClassroomsRoutes::register($app, new Controllers\Admin\ClassroomsController());
     Routes\Admin\CoursesRoutes::register($app, new Controllers\Admin\CoursesController());
     Routes\Admin\CMSRoutes::register($app, new Controllers\Admin\CMSController());
     Routes\Admin\StudentsRoutes::register($app, new Controllers\Admin\StudentsController());
+
+    //miscelaneous routes  here
     Routes\Admin\AdminRoutes::register($app, null, $db);
 }
 
-
+ //TEACHER ROUTES
 if (isset($_SESSION[USER_TYPE]) && $_SESSION[USER_TYPE] === TYPE_TEACHER) {
-    //TEACHER ROUTES
+   
     Routes\Teachers\CoursesRoutes::register($app, new Controllers\Teachers\CoursesController());
     Routes\Teachers\ModulesRoutes::register($app, new Controllers\Teachers\ModulesController());
     Routes\Teachers\SectionsRoutes::register($app, new Controllers\Teachers\SectionsController());
     Routes\Teachers\ClassroomsRoutes::register($app, new Controllers\Teachers\ClassroomsController());
     Routes\Teachers\GradesRoutes::register($app, new Controllers\Teachers\GradesController());
 
-    $app->get("/teachers", function ($req, $res) {
-        $data = [
-            "template" => "dashboard.php"
-        ];
-        $res->render("teachers/index", $data);
-        $res->status(HTTP_200_OK);
-    });
-
-    $app->get("/teachers/dashboard", function ($req, $res) {
-        $data = [
-            "template" => "dashboard.php"
-        ];
-        $res->render("teachers/index", $data);
-        $res->status(HTTP_200_OK);
-    });
-    $app->get("/bulletins/:id/delete", function ($req, $res) use ($db) {
-        $db->selectTable("bulletins");
-        $db->deleteById($req->params()["id"]);
-        $res->redirect("back");
-    });
-
-    $app->post("/bulletins", $app->form->sanitize, function ($req, $res) use ($db) {
-        try {
-            $query = "INSERT INTO bulletins (title, type,message, classroom_id)
-                VALUES (?, ?, ?,?)";
-
-            $statement = $db->conn()->prepare($query);
-
-            $data = [
-                $req->sanitized["title"],
-                $req->sanitized["type"],
-                $req->sanitized["message"],
-                $req->sanitized["classroom_id"],
-            ];
-            $statement->execute($data);
-            $statement->closeCursor();
-            $db->close();
-            setAlert("success", "Created a new notification");
-            $res->status(HTTP_201_CREATED);
-            $res->status(HTTP_301_MOVED_PERMANENTLY);
-            $res->redirect("/teachers/classrooms/" . $req->sanitized["classroom_id"],);
-        } catch (Exception $e) {
-            setAlert("danger", "Something went wrong! : " . $e->getMessage());
-        }
-    });
-
-    $app->get("/teachers/messages", function ($req, $res) use ($db) {
-        $query = [
-            "sql" => "SELECT * FROM students INNER JOIN users ON students.user_id = users.user_id",
-            "params" => []
-        ];
-
-        $messages = [];
-        if (isset($req->query()["from"])  && $req->query()["from"] == $_SESSION[USER_ID]) {
-            $messages = [
-                "sql" => "SELECT * FROM messages WHERE (sender_id = :sender_id AND recipient_id = :recipient_id) OR (sender_id = :recipient_id AND recipient_id = :sender_id)",
-                "params" => [
-                    "recipient_id" => $req->query()["to"],
-                    "sender_id" => $req->query()["from"]
-                ]
-            ];
-            $messages = $db->find($messages);
-        }
-
-        $data = [
-            "template" => "messages.php",
-            "students" => $db->find($query),
-            "messages" => $messages,
-        ];
-        $res->render("teachers/index", $data);
-        $res->status(HTTP_200_OK);
-    });
-
-    $app->get("/teachers/reports", function ($req, $res) use ($db) {
-        $progressQuery = [
-            "sql" => "SELECT * FROM progress
-            INNER JOIN classrooms ON progress.classroom_id = classrooms.classroom_id
-            WHERE classrooms.teacher_id = :id",
-            "params" => [
-                "id" => $_SESSION[TYPE_TEACHER]["teacher_id"]
-            ]
-        ];
-    
-        $progress = array_map(function ($elm) {
-            $total_points = (int) $elm["sections"] * 10;
-            $student_score = (int) $elm["total_score"];
-            $elm["grade"] = calculateGrade($total_points, $student_score);
-            $elm["max_score"] = $total_points;
-            return $elm;
-        }, $db->find($progressQuery));
-    
-        $data = [
-            "template" => "report.php",
-            "progress" => $progress,
-        ];
-        $res->render("teachers/index", $data);
-        $res->status(HTTP_200_OK);
-    });
-    
-    
+    //miscelaneous routes  here
+    Routes\Teachers\TeachersRoutes::register($app, null, $db);
 }
+
+
+//STUDENT ROUTES
 
 if (isset($_SESSION[USER_TYPE]) && $_SESSION[USER_TYPE] === TYPE_STUDENT) {
     Routes\Students\ClassroomsRoutes::register($app, new Controllers\Students\ClassroomsController());
     Routes\Students\ModulesRoutes::register($app, new Controllers\Students\ModulesController());
     Routes\Students\CoursesRoutes::register($app, new Controllers\Students\CoursesController());
-    Routes\Students\StudentsRoutes::register($app, null ,$db);
-
+    
+    //miscelaneous routes  here
+    Routes\Students\StudentsRoutes::register($app, null, $db);
 }
 
 
 
-$db = DB::new(DB_TYPE, DB_NAME, DB_PASSWORD, DB_DRIVER, DB_HOST, DB_USER);
-// un auth 
+// Other routes
+
 $app->get("/", function ($req, $res) use ($db) {
     $query = [
         "sql" => "SELECT * FROM homepage_cms 
@@ -225,7 +131,7 @@ $app->get("/register", function ($req, $res) {
     $res->status(HTTP_200_OK);
 });
 
-// Students routes only for students and admin-root available
+
 $app->get("/login", function ($req, $res) {
     $res->render("home/login");
     $res->status(HTTP_200_OK);
